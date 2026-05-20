2b4ee61e08262fbeb18350fa5e291407c92dee6f
NameCode51 <namecode51x@gmail.com>
2026-05-19 21:12:18 -0400

    NameCode51: Protocolo 0.3

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import discord
 import os
+import groq
 from dotenv import load_dotenv
 load_dotenv()
 # Clave De Tokens del Bot
 token = os.environ.get("TOKEN_BOT")
+ai_key = os.environ.get("API_AI_GROQ")
+
+#! AI
+ai = groq.Groq(api_key = ai_key)
+
 # Permits For The Bot
 permits = discord.Intents.default()
 
